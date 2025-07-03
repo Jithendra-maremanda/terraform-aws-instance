087d670acc8ccf466efaed657d665c41dd6eebd0
Jithendra-maremanda <jithendramaremanda@gmail.com>
2025-07-03 10:11:47 +0530

    your commit message

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "pub_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.this.private_ip
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
+output "instance_id" {
+  value = aws_instance.this.id
+  
+}
+
+
